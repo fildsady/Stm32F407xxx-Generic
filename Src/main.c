d5fea1cb80e1a1c31915d7def61c9f6ffa61e692
Anon <53999729+fildsady@users.noreply.github.com>
2026-06-16 20:32:32 +0700

    Initialize STM32F407xxx-Generic template with LL API, FreeRTOS, CMSIS-DSP, and VS Code configurations

---
+++ Src/main.c
@@ -0,0 +1,102 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_ll_bus.h"
+#include "stm32f4xx_ll_system.h"
+#include "stm32f4xx_ll_exti.h"
+#include "stm32f4xx_ll_cortex.h"
+#include "stm32f4xx_ll_utils.h"
+#include "stm32f4xx_ll_pwr.h"
+#include "stm32f4xx_ll_gpio.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "arm_math.h"
+
+void SystemClock_Config(void)
+{
+  /* Enable HSE (High Speed External Clock) */
+  LL_RCC_HSE_Enable();
+  while(LL_RCC_HSE_IsReady() != 1) { }
+
+  /* Set FLASH latency */
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_5);
+  if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_5) {
+    while(1);
+  }
+
+  /* Enable power clock and set voltage scaling to scale 1 (for max frequency) */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+
+  /* Configure PLL: HSE(8MHz) / 8 * 336 / 2 = 168MHz */
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 336, LL_RCC_PLLP_DIV_2);
+  LL_RCC_PLL_Enable();
+  while(LL_RCC_PLL_IsReady() != 1) { }
+
+  /* Set AHB/APB prescalers */
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_4);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_2);
+
+  /* Switch system clock to PLL */
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) { }
+
+  /* Update SystemCoreClock variable */
+  LL_SetSystemCoreClock(168000000);
+}
+
+void GPIO_Init(void)
+{
+  /* Enable GPIOD clock (LED on STM32F407 Discovery is PD12) */
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
+
+  /* Configure PD12 as Output */
+  LL_GPIO_SetPinMode(GPIOD, LL_GPIO_PIN_12, LL_GPIO_MODE_OUTPUT);
+  LL_GPIO_SetPinSpeed(GPIOD, LL_GPIO_PIN_12, LL_GPIO_SPEED_FREQ_LOW);
+  LL_GPIO_SetPinOutputType(GPIOD, LL_GPIO_PIN_12, LL_GPIO_OUTPUT_PUSHPULL);
+  LL_GPIO_SetPinPull(GPIOD, LL_GPIO_PIN_12, LL_GPIO_PULL_NO);
+}
+
+static void dsp_test_task(void *args)
+{
+    (void)args;
+
+    /* CMSIS-DSP Test Data */
+    float32_t a[4] = {1.0f, 2.0f, 3.0f, 4.0f};
+    float32_t b[4] = {2.0f, 2.0f, 2.0f, 2.0f};
+    float32_t result[4];
+
+    while (1) {
+        /* Test Vector Multiplication (utilizes FPU/SIMD) */
+        arm_mult_f32(a, b, result, 4);
+
+        /* Test Fast Math Sine (utilizes FPU) */
+        float32_t sin_val = arm_sin_f32(PI/4);
+        (void)sin_val; // suppress unused warning
+
+        LL_GPIO_TogglePin(GPIOD, LL_GPIO_PIN_12);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+int main(void)
+{
+	/* Configure the system clock to 168 MHz */
+	SystemClock_Config();
+
+	/* Initialize GPIO */
+	GPIO_Init();
+
+	/* Create DSP test task */
+	xTaskCreate(dsp_test_task, "DSP_Test", configMINIMAL_STACK_SIZE + 128, NULL, 2, NULL);
+
+	/* Start FreeRTOS scheduler */
+	vTaskStartScheduler();
+
+	while (1) {
+        /* Should never reach here */
+	}
+	return 0;
+}
