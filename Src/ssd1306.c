4a44bf30ffc3471c2f27da30bb036340b11ab55e
Anon <53999729+fildsady@users.noreply.github.com>
2026-06-17 00:47:22 +0700

    Set I2C Clock speed to 400kHz Fast Mode

--- Src/ssd1306.c
+++ Src/ssd1306.c
@@ -192,7 +192,7 @@ static void SSD1306_I2C_Init(void) {
     
     LL_I2C_InitTypeDef I2C_InitStruct = {0};
     I2C_InitStruct.PeripheralMode = LL_I2C_MODE_I2C;
-    I2C_InitStruct.ClockSpeed = 100000;
+    I2C_InitStruct.ClockSpeed = 400000;
     I2C_InitStruct.DutyCycle = LL_I2C_DUTYCYCLE_2;
     I2C_InitStruct.OwnAddress1 = 0;
     I2C_InitStruct.TypeAcknowledge = LL_I2C_ACK;
